a434102729e85602b191f2d942a7a8bc99a276ba
Ethan Durrant <emd519@yahoo.com>
2014-10-04 15:44:14 -0700

    made a blinky program that hopefully works

--- programs_asm/blinky_asm/main.asm
+++ programs_asm/blinky_asm/main.asm
@@ -4,6 +4,10 @@
 
 ;.def and .equ things
 
+.def regA = r16;
+.def regB = r17;
+.def regC = r18;
+
 ;.db things
 
 
@@ -11,13 +15,28 @@
 .org 0000
 	rjmp RESET;
 
+.org OVF0addr
+	rjmp TIM_OVF0;
 
 RESET:
 	;stuff do do at reset
-
+	
+	;ldi regA, (1<<PB1);
+	sbi DDRB, PB1;
+	ldi regA, (1<<CS02)|(1<<CS00);
+	out TCCR0B, regA;
+	ldi regA, (1<<TOIE0);
+	out TIMSK, regA;
+	sei;
 	rjmp MAIN; ;jump to main
 
 MAIN:
 	;do stuff
-
+	
+	nop;
+	rjmp MAIN;
 ;things
+
+TIM_OVF0:
+	sbi PINB, PB1;
+	reti;
