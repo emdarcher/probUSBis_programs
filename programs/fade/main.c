2d593773b0c3418d5939805723f84ed589494284
Ethan Durrant <emd519@yahoo.com>
2014-10-05 17:19:22 -0700

    going to start a fading LED using PWM project

---
+++ programs/fade/main.c
@@ -0,0 +1,26 @@
+
+
+#include "main.h"
+
+/* defines */
+
+/* function prototypes */
+
+void main(void){
+
+    /* init stuff */
+    
+    /* infinite loop */
+    while(1){
+        
+    }
+}
+
+/* function bodies */
+
+
+
+/* Interrupt Service Routines */
+
+
+
