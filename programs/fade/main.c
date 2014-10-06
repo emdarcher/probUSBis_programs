0b8489e402f56c084119b69064c9364c0878d99e
Ethan Durrant <emd519@yahoo.com>
2014-10-05 20:22:09 -0700

    adding some comments to the code

--- programs/fade/main.c
+++ programs/fade/main.c
@@ -1,4 +1,4 @@
-
+/* fades the led on PB1 */
 
 #include "main.h"
 
@@ -17,48 +17,47 @@ volatile uint8_t my_flags;
 void main(void){
 
     /* init stuff */
-
     set_clk_div16();
-    
     DDRB |= (1<<PB1);
-    
     init_tim0();
 
-    sei();     
+    sei();/* enable interrupts */     
     /* infinite loop */
     while(1){
-        
+       //do nothin' 
     }
 }
 
 /* function bodies */
 
 static inline void set_clk_div16(void){
-    CLKPR = (1<<CLKPCE);
-    CLKPR |= (1<<CLKPS2);
+/* sets the cpu clk to be the system clk/16 */
+    CLKPR = (1<<CLKPCE); //set CLK prescaler enable bit
+    CLKPR |= (1<<CLKPS2); // set the prescaler to clk/16
 }
 
 static inline void init_tim0(void){
-    
+/* stuff to initialize timer0 for things */
     TCCR0A |= (1<<WGM01)|(1<<WGM00); //set to fast PWM mode
     TCCR0A |= (1<<COM0B1); //set clear on compare match for OC0B
-
     TCCR0B |= (1<<CS02)|(1<<CS00); //set to clk/1024 prescaler 
-    
     TIMSK |= (1<<TOIE0); //enable timer0 overflow interrupt
-    
 }
 
 /* Interrupt Service Routines */
 
 ISR(TIM0_OVF_vect){
+/* Timer0 overflow interrupt service routine */
     
-    --pwm_fade_OCR0B;
-    OCR0B = (bit_is_set(my_flags, PWM_OCR0B_DIR_BIT)) ? (255-pwm_fade_OCR0B) : (pwm_fade_OCR0B) ; 
+    /* decrement the pwm value, this will go until zero, 
+    then jump to 255, then keep decrementing */
+    --pwm_fade_OCR0B; 
+    /* if the PWM direction flag is set, then output 255-pwm_value, else output pwm_value */
+    OCR0B = (bit_is_set(my_flags, PWM_OCR0B_DIR_BIT)) ? (255-pwm_fade_OCR0B) : pwm_fade_OCR0B; 
+    /* if the pwm_value has reached zero, then toggle the pwm direction flag */
     if(!pwm_fade_OCR0B){
         my_flags ^= (1<<PWM_OCR0B_DIR_BIT);
-    }
-    
+    } 
 }
 
 
